604443806f28fa27e5aabb6d25c71d2eb823eaa4
Eros Vieira <147876515+erosnoxx@users.noreply.github.com>
2024-03-15 18:05:15 -0300

    Delete app directory

--- app/controllers/api.py
+++
@@ -1,7 +0,0 @@
-from app.api.v1.auth.user import auth_api
-from app.api.v1.patients.patients import patient_api
-
-
-def init_app(app):
-    app.register_blueprint(auth_api)
-    app.register_blueprint(patient_api)
--- app/controllers/routes.py
+++
@@ -1,9 +0,0 @@
-from app.blueprints.auth.auth_bp import auth
-from app.blueprints.home.home_bp import home
-from app.blueprints.record.record_bp import record
-
-
-def init_app(app):
-    app.register_blueprint(auth)
-    app.register_blueprint(home)
-    app.register_blueprint(record)
